8e178297fa8d9045d5c4eceb04d76f7828fc294c
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-08-30 15:50:31 +0900

    add apache

---
+++ site-cookbooks/vim/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: vim
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "vim-enhancehd" do
+  action :install
+end
