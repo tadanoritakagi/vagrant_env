a89cfaaab74051fc3534dea4677b36a49c051d9a
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-08-31 05:36:10 +0900

    add noko success

--- site-cookbooks/ruby-env/recipes/default.rb
+++ site-cookbooks/ruby-env/recipes/default.rb
@@ -105,8 +105,8 @@ end
 end
 
 # nokogiriのインストール
-  execute "gem install nokogiri" do
-    command "/home/#{node['ruby-env']['user']}/.rbenv/shims/gem install nokogiri"
+  execute "gem install nokogiri -- --use-system-libraries" do
+    command "/home/#{node['ruby-env']['user']}/.rbenv/shims/gem install nokogiri -- --use-system-libraries"
     user node['ruby-env']['user']
     group node['ruby-env']['group']
     environment 'HOME' => "/home/#{node['ruby-env']['user']}"
