3e8f40a9d7c47824f6846e5978e8810948cab6d1
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-08-30 16:21:13 +0900

    improve apache httpd.conf

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -15,3 +15,12 @@ end
 service "httpd" do
   action [:enable, :start]
 end
+
+# apache設定ファイルhttpd.confの場所と場所の設定後のリロード
+template "httpd.conf" do
+  path "/etc/httpd/conf/httpd.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[httpd]'
+end
