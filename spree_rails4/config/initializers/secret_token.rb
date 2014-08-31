4cd212c850d4d4109a6d1c0e8e2b137ce525f49d
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-08-31 23:12:03 +0900

    basic end

---
+++ spree_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeRails4::Application.config.secret_key_base = '14af06d315d78c7dc1bb3fe568d5631c039978a191b29ab1ae60f9d3e3298e245b369fd5d641852114cbc683236feabf4a7f5ef3108b8909eb187a20ac4c428f'
