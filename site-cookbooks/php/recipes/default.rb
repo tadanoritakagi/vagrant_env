0d8f2bfebd9e16beed7b1f805e554b2ab56ebd13
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-09-13 03:19:51 +0900

    php added

---
+++ site-cookbooks/php/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: php
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w{php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
+   package p do
+     action :install
+   end
+end
