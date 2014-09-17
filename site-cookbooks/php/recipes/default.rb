d8c4381dcedef3ebce3773c2edba322febe13e15
Tadanori Takagi <tadanoritakagi0313@gmail.com>
2014-09-17 11:31:16 +0900

    php-pdo added

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-%w{php php-common php-mbstring php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
+%w{php php-common php-mbstring php-pdo php-xml php-devel php-process php-cli php-pear php-mysql mysql-server}.each do |p|
    package p do
      action :install
    end
